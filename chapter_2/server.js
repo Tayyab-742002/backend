c1f2204f6232213e6c7c9db88ebc7e324591c5bd
Muhammad Tayyab <muhammadtayyab742002@gmail.com>
2025-02-18 03:24:45 +0500

    chp 2 done

--- chapter_2/server.js
+++ chapter_2/server.js
@@ -2,11 +2,18 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+//Middleware
+
+app.use(express.json());
 //HTTP VERBS && Routes (or paths)
 app.get("/", (req, res) => {
   res.send("<h1>This is a website</h2>");
 });
+app.post("/", (req, res) => {
+  const body = req.body;
 
+  res.status(201).json(body);
+});
 app.listen(PORT, () => {
   console.log(`Server has started on http://localhost:${PORT}`);
 });
